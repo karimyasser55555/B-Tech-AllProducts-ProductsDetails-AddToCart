ebaaf4ee85d06a1f14db63478d85abf452044abd
Karim Yasser <104207447+karimyasser55555@users.noreply.github.com>
2023-04-08 17:48:09 +0200

    Add files via upload

---
+++ environments/environment.development.ts
@@ -0,0 +1,2 @@
+export const environment = {APIURL:'http://localhost:5011/api',ImageURl: 'http://localhost:5185/'
+};
---
+++ environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  APIURL:'http://localhost:5011/api',
+  ImageURl: 'http://localhost:5185'
+};
